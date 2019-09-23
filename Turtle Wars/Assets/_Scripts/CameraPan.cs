ec25e7109ddeb63fc57dd081c3f6c17f0b470749
Thomas Luckman <Tom.j.luckman@gmail.com>
2019-09-23 12:02:54 +1000

    Added camera script stuff
    +Added camera script, not functional yet.

---
+++ Turtle Wars/Assets/_Scripts/CameraPan.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+
+    public float panSpeed;
+
+    private float panDetect = 15f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MoveCamera();
+
+    }
+
+    void MoveCamera()
+    {
+        float moveX = Camera.main.transform.position.x;
+        float moveZ = Camera.main.transform.position.y;
+
+        float xPos = Input.mousePosition.x;
+        float yPos = Input.mousePosition.y;
+
+        if (Input.GetKey(KeyCode.A) || xPos > 0 && xPos < panDetect)
+        {
+            moveX -= panSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D) || xPos < Screen.width && xPos > Screen.width - panDetect)
+        {
+            moveX += panSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W) || yPos < Screen.height && yPos > Screen.height - panDetect)
+        {
+            moveZ += panSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S) || yPos > 0 && yPos < panDetect)
+        {
+            moveZ -= panSpeed;
+        }
+
+        Vector3 newPos = new Vector3(moveX, 0, moveZ);
+        Camera.main.transform.position = newPos;
+    }
+}
