83130396f3c3bb6d6f5a52bd37c734065330237a
Thomas Luckman <Tom.j.luckman@gmail.com>
2019-09-25 23:35:21 +1000

    Camera Movement controls 100% complete.
    + Click and Drag Camera Controls
    ~ Reworked Side-screen controls to disable click and drag controls if another one is active.

--- Turtle Wars/Assets/_Scripts/CameraPan.cs
+++ Turtle Wars/Assets/_Scripts/CameraPan.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class CameraPan : MonoBehaviour
 {
-
+    // Adjustable variables for X and Z movement
     public float panSpeed;
+    // Adjustable variable for Y movement
     public float scrollSpeed;
+    // Defines the distance from the edge of the screen the cursor has to be
+    private readonly float panDetect = 50f;
+    // Ensures that the 2 cameras don't lock the player into the one spot
+    private bool transition = true;
+    // Sets initial click point
+    private Vector3 dragOrigin;
+    // Sets the relative on how far the map moves vs cursor movement
+    public float dragSpeed;
 
-    private float panDetect = 50f;
-
-
+    // Sets bool that disables moveCamera whilst dragCamera is active
+    private bool moveDisable = false;
+    // Sets bool that disables dragCamera whilst moveCamera is active
+    private bool dragDisable = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,26 +31,36 @@ public class CameraPan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        MoveCamera();
+        // This method will be controlling the movement of the camera on the X, Y and Z axis
+        if (moveDisable == false){
+            MoveCamera();
+        }
+        if (dragDisable == false) {
+            DragCamera();
+        }
 
     }
 
     void MoveCamera()
     {
+        // defining the Camera location from the X, Y, Z positions to set a baseline
         float moveX = Camera.main.transform.position.x;
-        float moveY = Camera.main.transform.position.y;
+        float zoomY = Camera.main.transform.position.y;
         float moveZ = Camera.main.transform.position.z;
 
         float xPos = Input.mousePosition.x;
         float yPos = Input.mousePosition.y;
 
-        moveY += (-Input.GetAxis("Mouse ScrollWheel") * scrollSpeed);
+        zoomY += (-Input.GetAxis("Mouse ScrollWheel") * scrollSpeed);
 
-        if (moveY > 200)
-        {
-            moveY = 350;
-
+        if (zoomY >= 600 && transition == true) { // number here has to be bigger than the number given in second if
+            zoomY = 1300; // number here must be bigger than the second conditional if
+            transition = false;
+        }
+        
+        if (zoomY <= 900 && transition == false) { // number here has to be smaller than the number given above
+            zoomY = 500; // number here must be smaller than the first conditional if
+            transition = true;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow) || xPos > 0 && xPos < panDetect)
@@ -60,10 +81,41 @@ public class CameraPan : MonoBehaviour
             moveZ -= panSpeed;
         }
 
-        
+        if (Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.UpArrow)) || (Input.GetKey(KeyCode.RightArrow)) || (Input.GetKey(KeyCode.LeftArrow)) || xPos > 0 && xPos < panDetect || xPos < Screen.width && xPos > Screen.width - panDetect || yPos < Screen.height && yPos > Screen.height - panDetect || yPos > 0 && yPos < panDetect){
+            dragDisable = true;
+        }
+        else {
+            dragDisable = false;
+            //Debug.Log("Player isn't doing anything");
+        }
 
 
-        Vector3 newPos = new Vector3(moveX, moveY, moveZ);
+        Vector3 newPos = new Vector3(moveX, zoomY, moveZ);
+        //Debug.Log("moveX: " + moveX + ", moveY: " + zoomY + ", moveZ: " + moveZ);
         Camera.main.transform.position = newPos;
+    }
+
+    void DragCamera() {
+        // disables other movement
+        if (Input.GetMouseButtonDown(0)) {
+            moveDisable = true;
+            Debug.Log("Player started dragging");
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            moveDisable = false;
+            Debug.Log("Player stopped dragging");
+        }
+        // beginning of actual processing
+        if (Input.GetMouseButtonDown(0)) {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+        if (!Input.GetMouseButton(0)) return;
+
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+
+        transform.Translate(-move, Space.World);
     }
 }
