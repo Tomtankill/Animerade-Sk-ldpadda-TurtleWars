5a7e70b0130f98e9c0772d78638ceb3655730c71
Thomas Luckman <Tom.j.luckman@gmail.com>
2019-10-03 09:21:50 +1000

    Fog of War Experimentation
    + Fog of war Scene
    + Fog of war Directory
    + Fog of war shader script
    + _FogOfWar tag

---
+++ Turtle Wars/Assets/FogOfWarAssets/Revealer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revealer : MonoBehaviour
+{
+    public int radius;
+
+    private void Start()
+    {
+        FogOfWarManager.Instance.RegisterRevealer(this);
+    }
+
+}
+
+
+
+
+//https://tyler-white-pujb.squarespace.com/blog/2015/6/14/fog-of-war-in-unity-3d-part-one-of-three
