9c7c6acd5d374911a2cec1d5bace73b251427b4b
LD <ruggot1998@gmail.com>
2019-11-06 04:00:11 +0100

    Comment on the PriorityAI
    ~ comments in PriorityAI

--- Turtle Wars/Assets/_AI/PriorityAI.cs
+++ Turtle Wars/Assets/_AI/PriorityAI.cs
@@ -14,21 +14,29 @@ public class PriorityAI : MonoBehaviour
     // this function is this event will move all worker units with the enemy tag on it to target postion in units script
     public static event MasterAIPositionType moveWokerUnitsAIEvent;
 
-
+    // this is refrence the turntimer script (HAVEN'T ADDED SO THE AI CARES ABOUT TURNS)
     private TurnTimer turntime;
     private bool barrackBuild;
+
+    // this stores the AI's resources
     public float rScore1;
     public float rScore2;
+
+    // refrence the AI's units prefab so it can me instantiate it
     public GameObject myUnits;
     public Transform myBase;
 
-    // resources in the game
+    // list of the transform of resources in the map
     public static List<Transform> r1 = new List<Transform>(), r2 = new List<Transform>();
+
     // players units and building
     public static List<Transform> playerUnits = new List<Transform>(); 
     public static List<GameObject> AIUnits = new List<GameObject>();
+
+    // this is the acceptableDistance that a unit can find a enemy/resource
     public float acceptableDistance = 25f;
 
+    // sets the state of the AI, that changes what the AI will focus on
     public State state;
     public enum State
     {
@@ -36,14 +44,13 @@ public class PriorityAI : MonoBehaviour
         Attack,
         Defend
     }
-    // Start is called before the first frame update
+
     void Start()
     {
-
+        // CHECK with Tom how to setup the turntimer so AI dosen't cheat >:c
         //turntime = GetComponent<TurnTimer>();
-        //r1 = new List<Resource>();
-        //r2 = new List<Resource>();
 
+        // finds all gameobjects with the script <Resources> on it, then depending on what type of resource it adds it to that list
         foreach (Resource r in FindObjectsOfType<Resource>())
         {
             if (r.resourceType == ResourceType.r1)
@@ -54,6 +61,7 @@ public class PriorityAI : MonoBehaviour
                 r2.Add(r.transform);
         }
 
+        // finds all the gameobjects with the <Units> script on them, then add them to two seprets lists depending who is controlling them
         foreach (Units U in FindObjectsOfType<Units>())
         {
             if (U.whoControllsThis == Units.WhoControllsThis.Player)
@@ -67,12 +75,11 @@ public class PriorityAI : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
 
-        // switches state
+        // switches state and runs a function depending on what state it's in
         switch (state)
         {
             case State.Eco:
@@ -85,19 +92,10 @@ public class PriorityAI : MonoBehaviour
                 DefendMode();
                 break;
         }
-
-        //if (moveFighterUnitsAIEvent != null)
-        //{
-        //    moveFighterUnitsAIEvent(newTarget);
-        //}
-
-        // only if it's AI turn
-        //if (turntime.p2Turn == true)
-        //{
-
-        //}
+        // ADDS that AI checks for it's turn 
     }
 
+    // find the closest resources transform, then return the closest resource
     Transform GetNearestResource(List<Transform> resource)
     {
         Transform closest = null;
@@ -123,6 +121,7 @@ public class PriorityAI : MonoBehaviour
         return closest;
     }
 
+    // finds the closest player units transform, then return the target
     Transform GetNearestEnemy(List<Transform> playerunits)
     {
         Transform closest = null;
@@ -150,27 +149,31 @@ public class PriorityAI : MonoBehaviour
 
     void Eco()
     {
-        //Tell workers to gather resources
+        //Tell workers to gather the closes resource
+
         if (moveWokerUnitsAIEvent != null)
         {
             Debug.Log("Finding Resource");
+            // refrence the list of resources, but not in the GetNearestResource, is this the best way of doing it?
             moveWokerUnitsAIEvent(GetNearestResource(r1));
         }
 
-        //check if we can build barracks
+        //check if AI can build barracks
         if(rScore1 > 10 && barrackBuild == false)
         {
             print("IM READY TO BUILD A BARRACKS! UWU");
-
+            rScore1 -= 10f;
             barrackBuild = true;
             // add barracks spawnpOINT
         }
+        // if barrack is already build, create units at the barracks posistion
         else if (rScore1 > 10 && barrackBuild == true)
         {
             MakeUnits();
             rScore1 -= 10f;
         }
 
+        // if AIunits list is bigger then 7 then go into attack state
         if (AIUnits.Count > 7)
         {
             state = State.Attack;
@@ -178,11 +181,13 @@ public class PriorityAI : MonoBehaviour
 
     }   
 
+    // if target ain't null get the closest player units
     void AttackMode()
     {
 
         if (moveFighterUnitsAIEvent != null)
         {
+            // CHANGES this to the player units when Tom is done
             moveFighterUnitsAIEvent(GetNearestEnemy(playerUnits));
         }
     }
@@ -197,6 +202,7 @@ public class PriorityAI : MonoBehaviour
         Instantiate(myUnits, myBase.position, myBase.rotation);
         //Instantiate(myUnits, new Vector3(0, 0, 0), Quaternion.identity);
         AIUnits.Add(gameObject);
+        // CHANGES this into the Units script
         Debug.Log("Hey I'm with the crew" + AIUnits.Count);
     }
 }
