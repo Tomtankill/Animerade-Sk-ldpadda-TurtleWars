b3e7b374333d04ec1585ee889ff1469cc48e2ec1
CharlieT <1015816@student.sae.edu.au>
2019-12-02 23:31:04 +1100

    Pause Menu Testing
    Almost fully functioning Pause Menu 
    Requiring a SceneLoader for completed status

---
+++ Turtle Wars/Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool Cancel = false;
+    public bool PlayButton = false;
+    public GameObject Pause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Cancel") && Cancel == false)
+        {
+            Pause.SetActive(true);
+            Time.timeScale = 0;
+            Cancel = true;
+        }
+
+        else if (Input.GetButtonUp("Cancel") || PlayButton == true && Cancel == true)
+        {
+            Pause.SetActive(false);
+            Time.timeScale = 1;
+            Cancel = false;
+        }
+        
+    }
+}
