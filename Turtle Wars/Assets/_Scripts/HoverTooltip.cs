9935de525fa13e2b11990cd676db1540fb1aabac
Charlie <1015816@student.sae.du.au>
2019-12-03 12:50:13 +1100

    Added Tooltips + Updated pause menu
    + Updated Prefabs with Tool Tips
    + Added Hover Script
    + Added Event Trigger for the hover effect

---
+++ Turtle Wars/Assets/_Scripts/HoverTooltip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverTooltip : MonoBehaviour
+{
+    private bool mouseOver = false;
+    public GameObject ToolTip;
+
+
+    private void OnMouseEnter()
+    {
+        mouseOver = true;
+        ToolTip.SetActive(true);
+        print("Enter");
+    }
+    private void OnMouseExit()
+    {
+        mouseOver = false;
+        ToolTip.SetActive(false);
+        print("Exit");
+    }
+
+
+}
