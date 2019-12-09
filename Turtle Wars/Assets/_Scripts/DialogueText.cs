a585cab55566e860fc575367e947bf6bd2efb757
Thomas Luckman <Tom.j.luckman@gmail.com>
2019-12-10 00:43:41 +1100

    Dialogue script update

--- Turtle Wars/Assets/_Scripts/DialogueText.cs
+++ Turtle Wars/Assets/_Scripts/DialogueText.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class DialogueText : MonoBehaviour
 {
@@ -20,8 +21,22 @@ public class DialogueText : MonoBehaviour
     public float sentencestart;
     private float sentencewait;
     public Image next;
-    public GameObject cutsceneManager;
+    public GameObject cameraShot;
     private int runthrough;
+    private int countClicks;
+
+    // Camera Stuff
+    public GameObject shot1;
+    public GameObject shot2;
+    public GameObject shot3;
+    public GameObject shot4;
+    public GameObject shot5;
+    public GameObject shot6;
+    public GameObject shot7;
+    public GameObject shot8;
+    public GameObject shot9;
+    public GameObject shot10;
+    public GameObject shot11;
 
     //public GameObject FPC;  // Assign the First Person Controller to this in the Editor.
 
@@ -42,12 +57,127 @@ public class DialogueText : MonoBehaviour
                 next.gameObject.SetActive(true);
                 if (Input.GetMouseButtonDown(0))
                 {
+                    countClicks++;
                     NextSentence();
                     next.gameObject.SetActive(false);
                 }
             }
         }
+
+        ShotTransition();
     }
+
+    private void ShotTransition() {
+        switch (countClicks) {
+            case 1:
+                shot1.SetActive(true);
+                shot2.SetActive(false);
+                shot3.SetActive(false);
+                shot4.SetActive(false);
+                shot5.SetActive(false);
+                shot6.SetActive(false);
+                shot7.SetActive(false);
+                shot8.SetActive(false);
+                shot9.SetActive(false);
+                shot10.SetActive(false);
+                shot11.SetActive(false);
+                break;
+            case 7:
+                shot1.SetActive(false);
+                shot2.SetActive(false);
+                shot3.SetActive(false);
+                shot4.SetActive(false);
+                shot5.SetActive(false);
+                shot6.SetActive(false);
+                shot7.SetActive(false);
+                shot8.SetActive(false);
+                shot9.SetActive(false);
+                shot10.SetActive(false);
+                shot11.SetActive(false);
+                break;
+            case 13:
+                shot1.SetActive(false);
+                shot2.SetActive(false);
+                shot3.SetActive(false);
+                shot4.SetActive(false);
+                shot5.SetActive(false);
+                shot6.SetActive(false);
+                shot7.SetActive(false);
+                shot8.SetActive(false);
+                shot9.SetActive(false);
+                shot10.SetActive(false);
+                shot11.SetActive(false);
+                break;
+            case 20:
+                shot1.SetActive(false);
+                shot2.SetActive(false);
+                shot3.SetActive(false);
+                shot4.SetActive(false);
+                shot5.SetActive(false);
+                shot6.SetActive(false);
+                shot7.SetActive(false);
+                shot8.SetActive(false);
+                shot9.SetActive(false);
+                shot10.SetActive(false);
+                shot11.SetActive(false);
+                break;
+            case 25:
+                shot1.SetActive(false);
+                shot2.SetActive(false);
+                shot3.SetActive(false);
+                shot4.SetActive(false);
+                shot5.SetActive(false);
+                shot6.SetActive(false);
+                shot7.SetActive(false);
+                shot8.SetActive(false);
+                shot9.SetActive(false);
+                shot10.SetActive(false);
+                shot11.SetActive(false);
+                break;
+            case 33:
+                shot1.SetActive(false);
+                shot2.SetActive(false);
+                shot3.SetActive(false);
+                shot4.SetActive(false);
+                shot5.SetActive(false);
+                shot6.SetActive(false);
+                shot7.SetActive(false);
+                shot8.SetActive(false);
+                shot9.SetActive(false);
+                shot10.SetActive(false);
+                shot11.SetActive(false);
+                break;
+            case 40:
+                shot1.SetActive(false);
+                shot2.SetActive(false);
+                shot3.SetActive(false);
+                shot4.SetActive(false);
+                shot5.SetActive(false);
+                shot6.SetActive(false);
+                shot7.SetActive(false);
+                shot8.SetActive(false);
+                shot9.SetActive(false);
+                shot10.SetActive(false);
+                shot11.SetActive(false);
+                break;
+            case 45:
+                shot1.SetActive(false);
+                shot2.SetActive(false);
+                shot3.SetActive(false);
+                shot4.SetActive(false);
+                shot5.SetActive(false);
+                shot6.SetActive(false);
+                shot7.SetActive(false);
+                shot8.SetActive(false);
+                shot9.SetActive(false);
+                shot10.SetActive(false);
+                shot11.SetActive(false);
+                break;
+            default:
+                break;
+        }
+    }
+
     public void Dialoguestart() {
 
         StartCoroutine(Type());
